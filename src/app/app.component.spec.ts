04b5cca91a0b209493d977ba76cbce7cc2bf1404
zcastaneda <zcastaneda@bancodeoccidente.com.co>
2021-09-22 22:56:49 -0500

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ComercializadoraABC-Front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ComercializadoraABC-Front app dis runnindg!');
   });
 });
