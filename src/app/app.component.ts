539638dd4825c65dd7210b9683aae6c9e090f37b
zcastaneda <zcastaneda@bancodeoccidente.com.co>
2021-09-24 18:42:53 -0500

    Actualizacion Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./app.sb-admin-2.min.css','./app.all.min.css']
 })
 export class AppComponent {
   title = 'ComercializadoraABC-Front';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { LoguinComponent } from './components/loguin/loguin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent,
+    LoguinComponent
   ],
   imports: [
     BrowserModule
